f678b91981fbf0852d2391c856b65c2b2e596d89
anshul152003 <anshulgangrade2k3@gmail.com>
2025-06-14 16:11:33 +0530

    commit

---
+++ src/CodingSeeko/InbuiltFunc.java
@@ -0,0 +1,13 @@
+package CodingSeeko;
+
+public class InbuiltFunc {
+
+	public static void main(String[] args) {
+		int x = Integer.parseInt("123");
+		Integer x1 = Integer.valueOf("10110",2);
+		int y = x1.intValue();
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+}
